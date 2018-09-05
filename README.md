4816fbe6188b413790d565bd6407978254318966
lixianliang <lixianliang@corp.netease.com>
2018-09-05 17:11:42 +0800

    add sagemath tag

---
+++ README.md
@@ -0,0 +1,21 @@
+# otctf/env 
+---
+
+## 简要说明
+
+本 `repo` 为 [`otctf/env`](https://hub.docker.com/r/otctf/env) 的维护工程，其主要为 `ctf` 比赛提高各种相关的环境， 可以请搭配 `playground` 工程一起来使用
+
+目前维护的主要对象是 `template\Dockerfile.j2` ，其构成了所有`Docker Image tag` 的模板。
+
+## Tag 说明
+
+- `all-14.04`
+	- 提供基于 `ubuntu-14.04` 的『尽可能全』的通用环境
+	- 是唯一一个安装了 `php5` 及相关工具的环境
+	- 部分工具缺失，主要原因是对 `ruby` 版本的依赖
+- `all-16.04`
+	- 提供基于 `ubuntu-16.04` 的通用环境
+- `all-18.04`
+	- 提供基于 `ubuntu-18.04` 的通用环境
+- `sagemath`
+	- 提供基于 `sagemath/sagemath` 的通用环境，其基于 `ubuntu-16.04`
--- bin/build-all.sh
+++ bin/build-all.sh
@@ -3,10 +3,13 @@
 THIS_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd)"
 
 # zsteg, one_gadget, seccomp-tools requires Ruby version >= 2.1.0.
-$THIS_DIR/../src/main.py --template Dockerfile.j2 --name all-14.04 --params os=14.04 ignore_zsteg=1 ignore_one_gadget=1 ignore_seccomp_tools=1
+$THIS_DIR/../src/main.py --template Dockerfile.j2 --name all-14.04 --params base=ubuntu-14.04 ignore_zsteg=1 ignore_one_gadget=1 ignore_seccomp_tools=1
 
 # php5 is only supported on ubuntu-14.04
-$THIS_DIR/../src/main.py --template Dockerfile.j2 --name all-16.04 --params os=16.04 ignore_php5=1
+$THIS_DIR/../src/main.py --template Dockerfile.j2 --name all-16.04 --params base=ubuntu-16.04 ignore_php5=1
 
 # php5 is only supported on ubuntu-14.04
-$THIS_DIR/../src/main.py --template Dockerfile.j2 --name all-18.04 --params os=18.04 ignore_php5=1
+$THIS_DIR/../src/main.py --template Dockerfile.j2 --name all-18.04 --params base=ubuntu-18.04 ignore_php5=1
+
+# segamath version，which is based on ubuntu-16.04
+$THIS_DIR/../src/main.py --template Dockerfile.j2 --name sagemath --params base=sagemath ignore_php5=1
